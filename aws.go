49cf51dea42445f4ab48afbc376261f161c6d26d
Natty Linden <natty@lindenlab.com>
2026-09-24 22:15:40 +0000

    Use aws-sdk-go-v2 for AWS access

--- aws.go
+++ aws.go
@@ -1,53 +1,32 @@
 package main
 
 import (
+	"context"
 	"log"
-	"os"
 	"sync"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 )
 
 var (
 	once sync.Once
-	sess *session.Session
+	cfg aws.Config
 )
 
-// Singleton AWS Session
-func GetAwsSession() *session.Session {
+// GetAwsConfig returns a singleton AWS config for use with AWS services.
+func GetAwsConfig() aws.Config {
 	once.Do(func() {
-		log.Println("creating AWS session")
+		log.Println("creating AWS config")
 		var err error
-		sess, err = CreateAwsSession()
+		cfg, err = config.LoadDefaultConfig(
+			context.TODO(),
+			config.WithDefaultRegion("us-west-2"),
+		)
 		if err != nil {
 			panic(err)
 		}
-		log.Printf("session created in %s", *sess.Config.Region)
+		log.Printf("config created for %s", cfg.Region)
 	})
-	return sess
-}
-
-func CreateAwsSession() (*session.Session, error) {
-	// Attempt to resolve the AWS region. Region may not be automatically
-	// resolved in some environments such as in a container running on ECS.
-	region, regionPresent := os.LookupEnv("AWS_REGION")
-	if !regionPresent {
-		region, regionPresent = os.LookupEnv("AWS_DEFAULT_REGION")
-	}
-	if !regionPresent {
-		sess, err := session.NewSession()
-		if err != nil {
-			return sess, err
-		}
-		meta := ec2metadata.New(sess)
-		if meta.Available() {
-			region, _ = meta.Region()
-		}
-	}
-
-	return session.NewSession(&aws.Config{
-		Region: aws.String(region),
-	})
+	return cfg
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,24 @@ module github.com/secondlife/get-secret
 go 1.26.1
 
 require (
-	github.com/aws/aws-sdk-go v1.55.6
+	github.com/aws/aws-sdk-go-v2 v1.47.1
+	github.com/aws/aws-sdk-go-v2/config v1.33.6
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.50.1
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.78.1
 	golang.org/x/sys v0.42.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.20.6 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.20.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.5.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.8.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.5.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.19 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.14.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/signin v1.10.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.38.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.43.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.51.1 // indirect
+	github.com/aws/smithy-go v1.28.1 // indirect
 )
